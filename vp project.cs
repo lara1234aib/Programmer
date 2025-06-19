e2193f65f3b191f66c393bd17fdfc9bc423a825b
lara1234aib <laraibq12345@gmail.com>
2025-06-19 18:12:11 +0500

    Add files via upload

---
+++ vp project.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int A = 45;
+            int B = 55;
+            int sum = A + B;
+            Console.WriteLine("The sum of A and B IS " + sum);
+            int C = 700;
+            int D = 200;
+            int SUB = C - D;
+            Console.WriteLine("The subtraction of C and D is " + SUB);
+            int E = 10;
+            int F = 700;
+            int division = E / F;
+            Console.WriteLine("The division of E and F is " + division);
+            int G = 5;
+            int H = 4;
+            int multiplication = G * H;
+            Console.WriteLine("The multiplication of G and H is " + multiplication);
+
+        }
+    }
+}
